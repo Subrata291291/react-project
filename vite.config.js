0f8abd612b3796695f2f1ef4bfe2f3472929c8b6
Subrata <subrata291291@hmail.com>
2025-06-13 12:40:13 +0530

    Added base for Netlify in vite.confing file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import inject from '@rollup/plugin-inject';
 
 export default defineConfig({
+  base: '/', // IMPORTANT: Set base path for Netlify
   plugins: [
     react(),
     inject({
