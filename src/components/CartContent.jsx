694b125c258384706b706fa513eaa8381f439992
Subrata <subrata291291@hmail.com>
2025-06-14 14:45:29 +0530

    Chnages in stylesheet

--- src/components/CartContent.jsx
+++ src/components/CartContent.jsx
@@ -21,7 +21,7 @@ const CartContent = () => {
     <div>
       <section className="cart-area">
         <div className="container">
-          <div className="row">
+          <div className="row gx-lg-5">
             <div className="col-md-8">
               <div className="product-details-left" data-aos="fade-right">
                 <div className="product-heading">
